7ba9e1771fbbf17e62f2fd835f5f93804629f1d6
DavideBernardini <davidebernardini09@gmail.com>
2021-09-30 18:57:01 +0200

    stampati comics utilizzando un props

---
+++ src/components/Comic.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="comic">
+        <a href="#">
+            <img :src="details.thumb" :alt="details.series">
+            <h4>{{details.series}}</h4>
+        </a>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Comic',
+    props: ['details']
+}
+</script>
+
+<style scoped lang="scss">
+@import "../assets/style/variables.scss";
+.comic {
+            padding-top: 2.1875rem;
+            width: calc(100% / 6);
+            img {
+                height: 11.5625rem;
+                width: 11.5625rem;
+                object-fit: cover;
+                object-position: top;
+            }
+            h4 {
+                color: #fff;
+                padding-top: 1.25rem;
+                font-weight: normal;
+                font-size: 1.25rem;
+                width: 90%;
+                text-transform: uppercase;
+            }
+        }
+
+</style>
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,8 +1,13 @@
 <template>
     <main>
-        <div class="in_progress">
+        <div class="jumbo titans"></div>
+        <div class="comics">
             <div class="container">
-                <h2>__Content goes here__</h2>
+                <h2>CURRENT SERIES</h2>
+                <div class="comics_container">
+                    <Comic v-for="(comic, index) in myJason" :key="index" :details="comic"/>
+                </div>
+                <button>LOAD MORE</button>
             </div>
         </div>
         <UsefullLinks/>
@@ -10,12 +15,20 @@
 </template>
 
 <script>
+import json from '../components/dc-comics.json';
+import Comic from '../components/Comic';
 import UsefullLinks from '../components/UsefullLinks';
 
 export default {
     name: 'Main',
     components: {
-        UsefullLinks
+        UsefullLinks,
+        Comic
+    },
+    data() {
+        return {
+            myJason: json
+        }
     }
 }
 </script>
@@ -23,10 +36,39 @@ export default {
 <style scoped lang="scss">
 @import "../assets/style/variables.scss";
 
-.in_progress {
-    height: 130px;
+.jumbo.titans {
+    height: 25rem;
+    background-image: url('../assets/img/jumbotron.jpg');
+    background-size: cover;
+}
+.container {
+    flex-direction: column;
+    justify-content: center;
+}
+.comics {
+    height: 42.5rem;
     background-color: #1c1c1c;
     color: #fff;
-    line-height: 130px;
+    .container {
+        position: relative;
+        h2 {
+            font-size: 1.875rem;
+            padding: .9375rem 1.875rem;
+            background-color: $mainColor;
+            position: absolute;
+            top: 0;
+            transform: translateY(-50%);
+        }
+    }
+    &_container {
+        width: 100%;
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: center;
+    }
+    button {
+        display: block;
+    }
+
 }
 </style>
